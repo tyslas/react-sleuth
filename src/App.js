a3209b4f06504f5eaa4a171eb382316d4076d776
Jordan Reed <jordanr23@gmail.com>
2017-12-05 15:25:55 -0700

    speed test results active

--- src/App.js
+++ src/App.js
@@ -1,48 +1,106 @@
-import React, {
-  Component
-} from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Tester from './components/Tester'
 import History from './components/History'
 import Rank from './components/Rank'
+import speedTest from './speedTest'
 
-const data = {
-  car: {
-    make: 'BMW',
-    model: 'Civic',
-    color: 'red'
-  }
-}
+// http://ip-api.com/json/
+
+const api = '';
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      data: data
+      data: [],
+      testResults: {
+        ISP: '',
+        speed: 0
+      },
+      calculating: false
     }
   }
 
-  fun = (e) => {
+  // async componentDidMount() {
+  //   const response = await fetch(api)
+  //   const json = await response.json()
+  //   this.setState({data: json["_embedded"].tests})
+  // }
+  //
+  // async addItem(data){
+  //   const response = await fetch(api, {
+  //     method: 'POST',
+  //     body: JSON.stringify(data),
+  //     headers: {
+  //       'Content-Type': 'application/json',
+  //       'Accept': 'application/json'
+  //     }
+  //   })
+  //   this.componentDidMount();
+  // }
+
+  // fun = (e) => {
+  //   e.preventDefault();
+  //   console.log(e.target.owner.value);
+  //   var newData = data;
+  //   newData.car.owner = e.target.owner.value
+  //   newData.car.make = 'Honda';
+  //   this.setState({data: newData})
+  // }
+
+  runTest = (e) => {
+    this.setState({calculating: true});
     e.preventDefault();
-    console.log(e.target.owner.value);
-    var newData = data;
-    newData.car.owner = e.target.owner.value
-    newData.car.make = 'Honda';
-    this.setState({
-      data: newData
-    })
+    let newTest = this.state.testResults;
+    let allSpeeds = []
+    speedTest().then(speed => {
+      allSpeeds.push(speed)
+      speedTest().then(speed => {
+        allSpeeds.push(speed)
+        speedTest().then(speed => {
+          allSpeeds.push(speed)
+          speedTest().then(speed => {
+            allSpeeds.push(speed)
+            speedTest().then(speed => {
+              allSpeeds.push(speed)
+              speedTest().then(speed => {
+                allSpeeds.push(speed)
+                speedTest().then(speed => {
+                  allSpeeds.push(speed)
+                  speedTest().then(speed => {
+                    allSpeeds.push(speed)
+                    speedTest().then(speed => {
+                      allSpeeds.push(speed)
+                      speedTest().then(speed => {
+                        allSpeeds.push(speed)
+                        let data = [];
+                        allSpeeds.forEach(function(i){
+                          data.push(Number(i))
+                        })
+                           data.sort((a, b) => a - b);
+                           console.log(data);
+                           let lowMiddle = Math.floor((data.length - 1) / 2);
+                           let highMiddle = Math.ceil((data.length - 1) / 2);
+                           let median = (data[lowMiddle] + data[highMiddle]) / 2;
+
+                           newTest.speed = median
+                           this.setState({testResults: newTest,
+                           calculating: false})
+                          console.log(median);
+                        })})})})})})})})})})}
+
+
+
+    render() {
+      return (<div>
+        <header>Sleuth</header>
+        <Tester testSpeed={this.state.testResults.speed} runTest={this.runTest} calculating={this.state.calculating}/>
+        <History/>
+        <Rank/>
+      </div>);
+    }
   }
 
-  render() {
-    return ( <div>
-      <header>Sleuth</header>
-      <Tester data={this.state.data} fun={this.fun}/>
-      <History />
-      <Rank />
-      </div>
-    );
-  }
-}
-
-export default App;
+  export default App;
